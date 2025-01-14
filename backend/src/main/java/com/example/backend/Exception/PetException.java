8bdb1d1b990baea06602e17acf2acf4937249db0
PedroHenrique061 <phfla4255@gmail.com>
2025-01-14 18:31:13 -0300

    Adicionando um comentário na classe de Exception

--- backend/src/main/java/com/example/backend/Exception/PetException.java
+++ backend/src/main/java/com/example/backend/Exception/PetException.java
@@ -5,7 +5,7 @@ public class PetException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
 	public PetException(String mensagem) {
-		super(mensagem);
+		super(mensagem); //Função para inserir uma mensagem
 	}
 
 }
