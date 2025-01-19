d781639f0e7c392ecf244d3aa5bcb68eeddac706
AlexandreLJr <ubc183@gmail.com>
2025-01-19 17:23:57 -0300

    fix : arrumando routes

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/App';
 import Login from '../pages/Login/Cadastro';
+import Cadastrar from '../pages/Login/Login'
 import ConsultarPet from '../pages/Pets/ConsultarPet';
-import Calendario from '..pages/Pets/Calendario';
+import Calendario from '../pages/Pets/Calendario';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Cadastrar />} />
         <Route path="/pets" element={<ConsultarPet />} />
-        <Route path="/agendar" element={<Agendar />} /> 
+        <Route path="/agendar" element={<Calendario/>} /> 
       </Routes>
     </BrowserRouter>
   );
