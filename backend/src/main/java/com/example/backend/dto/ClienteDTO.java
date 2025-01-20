23683317ce2e0865d12bcc7835017b8e35ac1773
PedroHenrique061 <phfla4255@gmail.com>
2025-01-20 11:46:14 -0300

    Criação da funcionalidade de consultar dados do Cliente #32

---
+++ backend/src/main/java/com/example/backend/dto/ClienteDTO.java
@@ -0,0 +1,42 @@
+package com.example.backend.dto;
+
+public class ClienteDTO {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
--- backend/src/main/java/com/example/backend/service/ClienteService.java
+++ backend/src/main/java/com/example/backend/service/ClienteService.java
@@ -1,10 +1,14 @@
 package com.example.backend.service;
 
+import com.example.backend.Exception.ClienteException;
+import com.example.backend.dto.ClienteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.models.Cliente;
 import com.example.backend.repository.ClienteRepository;
+
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -18,5 +22,21 @@ public class ClienteService {
 
     public void cancelarCliente(Long clienteId) {
         clienteRepository.deleteById(clienteId);
+    }
+
+    public ClienteDTO consultarDadosCliente(Long clienteId) {
+        Optional<Cliente> clienteExist = clienteRepository.findById(clienteId);
+        if(clienteExist.isPresent()){ // verifica se existe o cliente
+            ClienteDTO clienteDto = new ClienteDTO(); // cria uma instância do clienteDto
+            clienteDto.setId(clienteExist.get().getId()); // tira do optional
+            clienteDto.setNome(clienteExist.get().getNome());
+            clienteDto.setTelefone(clienteExist.get().getTelefone());
+            clienteDto.setEndereco(clienteExist.get().getEndereco());
+
+            return clienteDto;
+        }
+        else{
+            throw new ClienteException("Cliente não existe!"); // mensagem de erro
+        }
     }
 }
